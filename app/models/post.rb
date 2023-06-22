90e41d11338e038e1a8cf3d9af544d6ea15aa264
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-06-22 22:36:41 +0300

    validations tests passed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
+# class Post < ApplicationRecord
+#     # validates :title, presence: true, clickbait: true
+#     # # validates :content, length: { minimum: 250 }
+#     # # validates :summary, length: { maximum: 250 }
+#     # # validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+# end
 class Post < ApplicationRecord
-end
+    validates :title, presence: true, uniqueness: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  
+    validate :title_contains_clickbait_phrases
+  
+    private
+  
+    def title_contains_clickbait_phrases
+      unless title&.match?(/(Won't Believe|Secret|Top \d|Guess)/)
+        errors.add(:title, "must be clickbait-y")
+      end
+    end
+  end
+  
